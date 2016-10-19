8601277c0168deb5fc67f00cab187a764c3515ed
Fernando Chamorro <fchamorro@mobile-tonic.com>
2016-10-19 16:45:35 -0300

    Fixed App Swift

--- MofilerAppObjC/MofilerAppObjC/AppDelegate.m
+++ MofilerAppObjC/MofilerAppObjC/AppDelegate.m
@@ -9,7 +9,7 @@
 #import "AppDelegate.h"
 #import <Mofiler/Mofiler-Swift.h>
 
-@interface AppDelegate ()
+@interface AppDelegate () <MofilerDelegate>
 
 @end
 
@@ -20,14 +20,26 @@
     Mofiler* mof = [Mofiler sharedInstance];
     
     [mof initializeWithAppKey:@"MY-APPKEY-HERE-IOS" appName:@"MyIosTestApplication" identity:@{@"username":@"johndoe"}];
-    
+    mof.delegate = self;
     mof.url = @"mofiler.com";
     [mof addIdentityWithIdentity:@{@"name":@"john doe"}];
     [mof addIdentityWithIdentity:@{@"email":@"john@doe.com"}];
     mof.useLocation = false;
     mof.useVerboseContext = true;
     
-    
+    [mof injectValueWithNewValue:@{@"mykey1":@"myvalue1"} expirationDateInMilliseconds:nil];
+    [mof injectValueWithNewValue:@{@"mykey2":@"myvalue2"} expirationDateInMilliseconds:[NSNumber numberWithFloat:100000000.0]];
+    [mof injectValueWithNewValue:@{@"mykey3":@"myvalue3"} expirationDateInMilliseconds:nil];
+    [mof injectValueWithNewValue:@{@"mykey4":@"myvalue4"} expirationDateInMilliseconds:[NSNumber numberWithFloat:100000000.0]];
+    [mof injectValueWithNewValue:@{@"mykey5":@"myvalue5"} expirationDateInMilliseconds:nil];
+    [mof injectValueWithNewValue:@{@"mykey6":@"myvalue6"} expirationDateInMilliseconds:nil];
+    [mof injectValueWithNewValue:@{@"mykey7":@"myvalue7"} expirationDateInMilliseconds:[NSNumber numberWithFloat:100000000.0]];
+    [mof injectValueWithNewValue:@{@"mykey8":@"myvalue8"} expirationDateInMilliseconds:nil];
+    [mof injectValueWithNewValue:@{@"mykey9":@"myvalue9"} expirationDateInMilliseconds:nil];
+    [mof injectValueWithNewValue:@{@"mykey10":@"myvalue10"} expirationDateInMilliseconds:nil];
+    [mof injectValueWithNewValue:@{@"mykey11":@"myvalue11"} expirationDateInMilliseconds:nil];
+    [mof injectValueWithNewValue:@{@"mykey12":@"myvalue12"} expirationDateInMilliseconds:nil];
+    [mof injectValueWithNewValue:@{@"mykey13":@"myvalue13"} expirationDateInMilliseconds:nil];
     
     [mof flushDataToMofiler];
     
@@ -39,4 +51,13 @@
     return YES;
 }
 
+- (void) responseValueWithKey:(NSString *)key identityKey:(NSString *)identityKey identityValue:(NSString *)identityValue value:(NSDictionary<NSString *,id> *)value {
+    NSLog(@"%@", value);
+}
+
+- (void) errorOcurredWithError:(NSString *)error userInfo:(NSDictionary<NSString *,NSString *> *)userInfo {
+    NSLog(@"%@", error);
+}
+
 @end
+
